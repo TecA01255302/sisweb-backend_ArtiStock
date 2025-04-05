ec64f14c2f9aac5733834321abc9e4df36432499
Sasha Valdes <sashavalj@gmail.com>
2025-04-04 23:12:29 -0700

    Fix: relation n:n between product and tags

--- src/connection/connection.ts
+++ src/connection/connection.ts
@@ -1,3 +1,28 @@
+// import { Sequelize } from "sequelize-typescript";
+// import { Product } from "../models/product";
+// import { User } from "../models/user";
+// import { Tag } from "../models/tag";
+// import { Tag_Product } from "../models/tag_product";
+
+// const connection = new Sequelize({
+//     database: 'artistock_db',
+//     dialect: 'mysql',
+//     username: 'artistock_user',
+//     password: 'artistock2608',
+//     storage: ':memory:',
+//     models: [Product, User, Tag, Tag_Product]
+// });
+
+// async function connectionDB() {
+//     try {
+//         await connection.sync();
+//     } catch (e) {
+//         console.log(e);
+//     }
+// }
+
+// export default connectionDB;
+
 import { Sequelize } from "sequelize-typescript";
 import { Product } from "../models/product";
 import { User } from "../models/user";
@@ -5,11 +30,8 @@ import { Tag } from "../models/tag";
 import { Tag_Product } from "../models/tag_product";
 
 const connection = new Sequelize({
-    database: 'artistock_db',
-    dialect: 'mysql',
-    username: 'artistock_user',
-    password: 'artistock2608',
-    storage: ':memory:',
+    dialect: 'sqlite',
+    storage: 'database.sqlite', 
     models: [Product, User, Tag, Tag_Product]
 });
 
